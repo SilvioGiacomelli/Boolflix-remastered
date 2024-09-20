3178fd089c62f8f6090aa4ecf4b340f78516572c
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-09-20 11:02:29 +0200

    Add Vue.volar extension recommendation

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+import axios from 'axios';
+import { store } from './data/store';
+
+export default {
+
+  components:{
+    Header, 
+    Main, 
+    Footer,
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+
+<template>
+  <Header/>
+
+  <Main/>
+
+  <Footer/>
+</template>
+
+<style lang="scss">
+@use './assets/scss/main.scss';
+</style>
